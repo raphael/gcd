0f81a79417e0e0ccb1b41be28d46eb43af406820
Raphael Simon <raphael@rightscale.com>
2015-03-18 16:28:03 -0700

    Initial commit

---
+++ gcd.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+)
+
+var debug = false
+
+func main() {
+	name := ""
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	if len(os.Args) > 2 {
+		debug = (os.Args[2] == "--debug" || os.Args[2] == "-d")
+	}
+	abs := path.Join(os.Getenv("HOME"), "src", "go", "src")
+	if name != "" {
+		match := &FileMatch{name: name, path: abs}
+		filepath.Walk(abs, match.Find)
+		abs = match.path
+	}
+	fmt.Println(abs)
+}
+
+type FileMatch struct {
+	name string
+	path string
+}
+
+func (f *FileMatch) Find(path string, info os.FileInfo, err error) error {
+	fname := info.Name()
+	if fname == f.name {
+		f.path = path
+		return fmt.Errorf("done")
+	}
+	if fname == "_workspace" || fname == ".git" || fname == ".hg" || fname == ".bundle" {
+		return filepath.SkipDir
+	}
+	if debug {
+		fmt.Println(path + "/" + info.Name())
+	}
+
+	return nil
+}
